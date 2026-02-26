2e5af454733ae8f8ee6db7d5b7b73a9ac40506cd
Ansh Mathur <infiplexity@gmail.com>
2026-02-26 14:52:28 +0530

    3

--- src/PalindromChecker.java
+++ src/PalindromChecker.java
@@ -2,20 +2,18 @@ import java.util.Stack;
 
 public class PalindromChecker {
   public static void main(String[] args) {
-
     String input = "madam";
 
-    boolean isPalindrome = true;
+    String reversed = "";
 
-    for (int i = 0; i < input.length() / 2; i++) {
-
-      if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-        isPalindrome = false;
-        break;
-      }
+    for (int i = input.length() - 1; i >= 0; i--) {
+      reversed = reversed + input.charAt(i);
     }
 
+    boolean isPalindrome = input.equals(reversed);
+
     System.out.println("Input text: " + input);
+    System.out.println("Reversed text: " + reversed);
     System.out.println("Is it a Palindrome? : " + isPalindrome);
   }
 }
