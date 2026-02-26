c90836e3cfe1df90d5ad345d74ac5c944748c482
Ansh Mathur <infiplexity@gmail.com>
2026-02-26 14:44:59 +0530

    Init

--- src/PalindromChecker.java
+++ src/PalindromChecker.java
@@ -2,24 +2,9 @@ import java.util.Stack;
 
 public class PalindromChecker {
   public static void main(String[] args) {
-    String input = "noon";
-
-    Stack<Character> stack = new Stack<>();
-
-    for (char c : input.toCharArray()) {
-      stack.push(c);
-    }
-
-    boolean isPalindrome = true;
-
-    for (char c : input.toCharArray()) {
-      if (c != stack.pop()) {
-        isPalindrome = false;
-        break;
-      }
-    }
-
-    System.out.println("Input : " + input);
-    System.out.println("Is Palindrome? : " + isPalindrome);
+    System.out.println("Welcome to the Palindrome Checker Management System");
+    System.out.println("Version : 1.0");
+    System.out.println("System initialized successfully.");
   }
+
 }
